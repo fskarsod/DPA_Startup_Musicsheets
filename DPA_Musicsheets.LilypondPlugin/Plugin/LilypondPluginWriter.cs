ece8cf975311e84e0d58044940e895ddae272de5
Ivo Hendriks <ihendrik1@student.avans.nl>
2016-10-10 23:35:57 +0200

    Removed old parsing and implemented dictionaries and simpler methods.
    WIP

--- DPA_Musicsheets.LilypondPlugin/Plugin/LilypondPluginWriter.cs
+++ DPA_Musicsheets.LilypondPlugin/Plugin/LilypondPluginWriter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using DPA_Musicsheets.Core.Builder;
 using DPA_Musicsheets.Core.Interface;
 using DPA_Musicsheets.Core.Model;
+using DPA_Musicsheets.Core.Model.Enum;
 using DPA_Musicsheets.LilypondPlugin.Enum;
 
 namespace DPA_Musicsheets.LilypondPlugin.Plugin
@@ -16,24 +17,22 @@ namespace DPA_Musicsheets.LilypondPlugin.Plugin
             var elements = SplitSource(source);
             var track = new TrackBuilder();
 
-            for (var i = 0; i < elements.Count(); i++)
+            foreach (var e in elements)
             {
-                switch (ToKeyword(elements[i]))
+                switch (ToKeyword(e))
                 {
                     case LilypondKeyword.None:
-                        break;
-                    case LilypondKeyword.Relative:
-                        break;
-                    case LilypondKeyword.Clef:
-                        break;
-                    case LilypondKeyword.Tempo:
+                        AddNoteToTrack(e);
                         break;
                     case LilypondKeyword.Time:
+                        SetTimeSignature(e);
                         break;
+                    case LilypondKeyword.Relative:
+                    case LilypondKeyword.Clef:
+                    case LilypondKeyword.Tempo:
                     case LilypondKeyword.Repeat:
-                        break;
                     case LilypondKeyword.Alternative:
-                        break;
+                        throw new NotImplementedException();
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
@@ -42,7 +41,17 @@ namespace DPA_Musicsheets.LilypondPlugin.Plugin
             return new Sheet();
         }
 
-        private void SetTimeSignature(TimeSignature timeSignature)
+        private void AddNoteToTrack(string noteString)
+        {
+            var pitch = ToPitch("" + noteString[0]);
+            var accidental = ToAccidental(noteString.Substring(1, 2));
+            if (noteString[1] == ',' || noteString[1] == '\'')
+            {
+                // up the octave // or count , or '  in string
+            }
+        }
+
+        private void SetTimeSignature(string timeSignatureString)
         {
             
         }
@@ -55,24 +64,48 @@ namespace DPA_Musicsheets.LilypondPlugin.Plugin
 
         private static LilypondKeyword ToKeyword(string s)
         {
-            switch (s)
-            {
-                case @"\relative":
-                    return LilypondKeyword.Relative;
-                case @"\clef":
-                    return LilypondKeyword.Clef;
-                case @"\tempo":
-                    return LilypondKeyword.Tempo;
-                case @"\time":
-                    return LilypondKeyword.Time;
-                case @"\repeat":
-                    return LilypondKeyword.Repeat;
-                case @"\alternative":
-                    return LilypondKeyword.Alternative;
-                default:
-                    // TODO: Possibly handle this better.
-                    return LilypondKeyword.None;
-            }
+            return LilypondKeywordDictionary.ContainsKey(s.ToLower()) ? LilypondKeywordDictionary[s] : LilypondKeyword.None;
         }
+
+        private static Pitch? ToPitch(string s)
+        {
+            if (PitchDictionary.ContainsKey(s.ToLower()))
+                return PitchDictionary[s];
+
+            return null;
+        }
+
+        private static Accidental ToAccidental(string s)
+        {
+            return AccidentalDictionary.ContainsKey(s.ToLower()) ? AccidentalDictionary[s] : Accidental.Natural;
+        }
+
+        private static readonly IDictionary<string, LilypondKeyword> LilypondKeywordDictionary = new Dictionary<string, LilypondKeyword>
+        {
+            { @"\relative",    LilypondKeyword.Relative },
+            { @"\clef",        LilypondKeyword.Clef },
+            { @"\tempo",       LilypondKeyword.Tempo },
+            { @"\time",        LilypondKeyword.Time },
+            { @"\repeat",      LilypondKeyword.Repeat },
+            { @"\alternative", LilypondKeyword.Alternative }
+        };
+
+        private static readonly IDictionary<string, Pitch> PitchDictionary = new Dictionary<string, Pitch>
+        {
+            { "a", Pitch.A },
+            { "b", Pitch.B },
+            { "c", Pitch.C },
+            { "d", Pitch.D },
+            { "e", Pitch.E },
+            { "f", Pitch.F },
+            { "g", Pitch.G }
+        };
+
+        // TODO: Add default (don't know if lilypond supports it)
+        private static readonly IDictionary<string, Accidental> AccidentalDictionary = new Dictionary<string, Accidental>
+        {
+            { "is", Accidental.Sharp},
+            { "es", Accidental.Flat }
+        };
     }
 }
